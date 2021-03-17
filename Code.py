ea3595eaaafbcbd04378a09a2756ae4bfdeb7467
Andrajo <62557131+Andrajo@users.noreply.github.com>
2021-03-17 19:53:29 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,197 +1,173 @@
-# Write your code here
 import random
 import sqlite3
 
 
-def Luhn_Algorithm(credit):
-    copy_credit=credit
-    suma=0
-    for i in range(len(copy_credit)):
-        if i%2==0:
-            if 2*int(copy_credit[i])>9:
-                suma+=2*int(copy_credit[i])-9
-            else:
-                suma+=2*int(copy_credit[i])
-        elif i%2!=0:
-            suma+=int(copy_credit[i])
-    if suma%10==0:
-        return True
-    return False
+class Account:
+    IIN = 400000
+
+    def __init__(self):
+
+        self.pin = str(random.sample([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4)).strip("[]").replace(',', '').replace(' ', '')
+        self.card_number = str(Account.luhn_algorithm(self)).strip("[]").replace(',', '').replace(' ', '')
+
+    def luhn_algorithm(self):
+        card = [4, 0, 0, 0, 0, 0] + random.sample([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 9)
+        temp = card.copy()
+        index = 0
+        for digit in temp:
+            if (index + 1) % 2 != 0:
+                temp[index] = digit * 2
+            index += 1
+        index = 0
+        for digit in temp:
+            if digit > 9:
+                temp[index] = digit - 9
+            index += 1
+        total = sum(temp)
+        card.append((total * 9) % 10)
+        return card
+
+    def get_card_number(self):
+        return self.card_number
+
+    def get_pin(self):
+        return self.pin
 
 
+class Bank:
+    def __init__(self):
+        self.Accounts = []
+        self.Accounts_details = {}
+        self.conn = sqlite3.connect('card.s3db')
+        self.cur = self.conn.cursor()
+        self.cur.execute('''CREATE TABLE IF NOT EXISTS card (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            number TEXT NOT NULL,
+            pin TEXT NOT NULL,
+            balance INTEGER DEFAULT 0
+            );
+            ''')
+        self.conn.commit()
 
-def card_creation():
-    credit=['400000']
-    seventh=random.choice(range(100000000,999999999))
-    while accounts.count(seventh)!=0:
-        seventh=random.choice(range(100000000,999999999))
-    accounts.append(seventh)
-    credit.append(str(seventh))
-    suma=0
-    copy_credit=''.join(credit)
-    for i in range(len(copy_credit)):
-        if i%2==0:
-            if 2*int(copy_credit[i])>9:
-                suma+=2*int(copy_credit[i])-9
-            else:
-                suma+=2*int(copy_credit[i])
-        elif i%2!=0:
-            suma+=int(copy_credit[i])
-    if suma%10==0:
-        credit.append('0')
-    else:
-        credit.append(str(10-(suma%10)))
-    credit=''.join(credit)
-    return credit
-
-
-
-
-def pin_creation():
-    pin=str(random.randint(1000,10000))
-    return pin
-
-
-def validity(credit,pin):
-    ok=-1
-    if credit in credit_cards:
-        ok=0
-        if credit_cards[credit]==pin:
-            ok=1
-    if ok==-1 or ok==0:
-        return False
-    if ok==1:
-        return True
-
-
-
-
-connnection=sqlite3.connect('card.s3db')
-cursor=connnection.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS card (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        number TEXT,
-        pin TEXT,
-        balance INTEGER DEFAULT 0
-    )""")
-connnection.commit()
-
-
-
-
-def second_comands(credit):
-    second_command=int(input("""1. Balance
-2. Add income
-3. Do transfer
-4. Close account
-5. Log out
-0. Exit\n"""))
-    breakup=0
-    while second_command!=5:
-        if second_command==0:
-            breakup=1
-            break
-        if second_command==1:
-            print("Balance: {}".format(credit_money[credit]))
-        if second_command==2:
-            add=int(input("Enter income:\n"))
-            print("Income was added!")
-            credit_money[credit]+=add
-            for i in credits_ids.keys():
-                if i==credit:
-                    poz=credits_ids[i]
-                    break
-            cursor.execute("UPDATE card SET balance = {} WHERE id = {}".format(credit_money[credit],poz))
-            connnection.commit()
-        if second_command==3:
-            second_credit=input("Transfer\nEnter card number:\n")
-            if second_credit==credit:
-                print("You can't transfer money to the same account!")
-            else:
-                if Luhn_Algorithm(second_credit)==False:
-                    print("Probably you made mistake in the card number. Please try again!")
+    def run(self):
+        while True:
+            choice = input('1. Create an account \n2. Log into account\n0. Exit\n')
+            if choice == '1':
+                Bank.create_account(self)
+            elif choice == '2':
+                card = input('Enter your card number:\n')
+                pin = input('Enter your PIN:\n')
+                if Bank.check_account(self, card, pin):
+                    print('You have successfully logged in!')
+                    Bank.login(self, card)
                 else:
-                    if second_credit in credit_cards:
-                        add=int(input("Enter how much money you want to transfer:\n"))
-                        if add>credit_money[credit]:
-                            print("Not enough money!")
-                        else:
-                            print("Success!")
-                            credit_money[credit]-=add
-                            credit_money[second_credit]+=add
-                            for i in credits_ids.keys():
-                                if i==credit:
-                                    poz=credits_ids[i]
-                                    break
-                            cursor.execute("UPDATE card SET balance = {} WHERE id = {}".format(credit_money[credit],poz))
-                            connnection.commit()
-                            for i in credits_ids.keys():
-                                if i==second_credit:
-                                    poz=credits_ids[i]
-                                    break
-                            cursor.execute("UPDATE card SET balance = {} WHERE id = {}".format(credit_money[second_credit],poz))
-                            connnection.commit()
-                            #cursor.execute("SELECT * FROM card")
-                            #rows=cursor.fetchall()
-                            #for i in rows:
-                            #    print(i)
-                    else:
-                        print("Such a card does not exist.")
-        if second_command==4:
-            print("The account has been closed!")
-            cursor.execute("DELETE FROM card WHERE number = {}".format(credit))
-            connnection.commit()
-            break
-        second_command=int(input("""1. Balance
-2. Add income
-3. Do transfer
-4. Close account
-5. Log out
-0. Exit\n"""))
-        if command==5:
-            print("You have successfully logged out!")
-    return breakup
+                    print('Wrong card number or PIN!')
+            else:
+                print('Bye!')
+                exit()
+
+    def check_account(self, card_number, pin):
+        self.cur.execute('SELECT * FROM card')
+        bank_data = self.cur.fetchall()
+        for a in bank_data:
+            if a[1] == card_number:
+                if a[2] == pin:
+                    return True
+                else:
+                    return False
+
+    def check_card(self, card_number):
+        self.cur.execute('SELECT * FROM card')
+        bank_data = self.cur.fetchall()
+        for a in bank_data:
+            if a[1] == card_number:
+                return True
+        return False
+
+    def check_receiver(self, card_number):
+        temp = list(card_number).copy()
+        check_digit = temp.pop()
+        index = 0
+        for digit in temp:
+            if (index + 1) % 2 != 0:
+                temp[index] = int(digit) * 2
+            index += 1
+        index = 0
+        for digit in temp:
+            if int(digit) > 9:
+                temp[index] = int(digit) - 9
+            index += 1
+        total = 0
+        for digit in temp:
+            total += int(digit)
+        return int(check_digit) == ((total * 9) % 10)
+
+    def create_account(self):
+        a = Account()
+        self.cur.execute(f"INSERT INTO card(number, pin, balance) VALUES({a.get_card_number()}, {a.get_pin()}, 0)")
+        self.conn.commit()
+        print('Your card number:')
+        print(a.get_card_number() + '\n')
+        print('Your card PIN:')
+        print(a.get_pin() + '\n')
+
+    def add_income(self, card_number, amount):
+        self.cur.execute(f'SELECT * FROM card WHERE number = {card_number}')
+        card = self.cur.fetchall()
+        self.cur.execute(f'UPDATE card SET balance = {card[0][3] + amount} WHERE number = {card_number}')
+        self.conn.commit()
+
+    def withdraw(self, card_number, amount):
+        self.cur.execute(f'SELECT * FROM card WHERE number = {card_number}')
+        card = self.cur.fetchall()
+        self.cur.execute(f'UPDATE card SET balance = {card[0][3] - amount} WHERE number = {card_number}')
+        self.conn.commit()
+
+    def transfer(self, from_, to):
+        if from_ == to:
+            print('You can\'t transfer money to the same account!')
+        elif not Bank.check_receiver(self, to):
+            print('Probably you made mistake in the card number. Please try again!')
+        elif not Bank.check_card(self, to):
+            print("Such a card does not exist.")
+        else:
+            amount = int(input('Enter how much money you want to transfer:'))
+            if amount > Bank.get_balance(self, from_):
+                print('Not enough money!')
+            else:
+                Bank.add_income(self, to, amount)
+                Bank.withdraw(self, from_, amount)
+                print('Success!')
+
+    def get_balance(self, card_number):
+        self.cur.execute(f'SELECT * FROM card WHERE number = {card_number}')
+        card = self.cur.fetchall()
+        return card[0][3]
+
+    def login(self, card_number):
+        while True:
+            choice = input('1. Balance \n2. Add income\n3. Do transfer\n4. Close account\n5. Log out\n0. Exit\n')
+            if choice == '1':
+                print('Balance:', Bank.get_balance(self, card_number))
+            elif choice == '2':
+                amount = int(input('Enter income:'))
+                Bank.add_income(self, card_number, amount)
+            elif choice == '3':
+                receiver = input('Enter card number:')
+                Bank.transfer(self, card_number, receiver)
+            elif choice == '4':
+                self.cur.execute(f'DELETE FROM card WHERE number = {card_number}')
+                self.conn.commit()
+                print('The account has been closed!')
+                break
+            elif choice == '5':
+                print('You have successfully logged out!')
+                break
+            else:
+                print('Bye!')
+                exit()
 
 
+B = Bank()
 
-
-
-command=int(input("""1. Create an account
-2. Log into account
-0. Exit\n"""))
-accounts=[]
-credit_cards= {}
-credit_money={}
-credits_ids={}
-breakup=0
-while command!=0:
-    if command==1:
-        print("Your card has been created")
-        credit=card_creation()
-        print(credit)
-        print("Your card PIN:")
-        pin=pin_creation()
-        print(pin)
-        credit_cards[credit]=pin
-        credit_money[credit]=0
-        cursor.execute("INSERT INTO card(number, pin, balance) VALUES ({}, {}, {})".format(credit,credit_cards[credit],credit_money[credit]))
-        connnection.commit()
-        credits_ids[credit]=cursor.lastrowid
-    if command==2:
-        credit=input("Enter your card number:")
-        pin=input("Enter your PIN:")
-        x=validity(credit,pin)
-        if x==False:
-            print("Wrong card number or PIN!")
-        if x==True:
-            print("You have successfully logged in!")
-            breakup=second_comands(credit)
-        if command==0:
-            break
-    if breakup==1:
-        break
-    command=int(input("""1. Create an account
-2. Log into account
-0. Exit\n"""))
-connnection.close()
-print("Bye!")
+B.run()
